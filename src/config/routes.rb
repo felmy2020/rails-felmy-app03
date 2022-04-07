ef1c8f705e779eff520747f9cc53e72bf402b84a
felmy2020 <fyen.work@gmail.com>
2022-04-07 22:07:38 +0900

    ユーザ登録追加

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'tops#index'
+  devise_for :users
+
+  root 'tops#index'
+  resources :tweets, only: %i(index new create)
+
 end
